215e63cf323faec28f20e4a567a786f3a7764549
emddeveloper <emd.developer@gmail.com>
2021-10-10 00:04:50 +0530

    component generation done

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'app-server',
+  templateUrl:'../server/server.component.html',
+  styleUrls:['../server/server.component.css']
+})
+export class ServerComponent {
+  serverName:string='SK'
+}
